88925472bb69f67c8f130b78e0a052ab01c2b1f0
Avinash Reddy <cassilasreddythemostwanted3108@gmail.com>
2019-09-04 13:10:00 +0530

    Update updater.py
    I heard this works with heroku + docker, let's see if it's true...
    
    Co-authored-by: Ayra Hikari <AyraHikari@gmail.com>

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -7,13 +7,14 @@
 This module updates the userbot based on Upstream revision
 """
 
-from os import remove, execl
+from os import remove, execl, mkdir, rename
+from shutil import rmtree
 import sys
 
 from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 
-from userbot import CMD_HELP
+from userbot import CMD_HELP, BOTLOG, BOTLOG_CHATID
 from userbot.events import register, errors_handler
 
 
@@ -27,11 +28,29 @@ async def gen_chlog(repo, diff):
 
 async def is_off_br(br):
     off_br = ['sql-extended', 'sql-dirty']
-    for k in off_br:
-        if k == br:
-            return 1
+    if br in off_br:
+        return True
     return
 
+# Kanged from : https://github.com/AyraHikari/Nana-TgBot/blob/master/nana/modules/updater.py
+async def initial_git(repo):
+	isexist = os.path.exists('userbot-old')
+	if isexist:
+		rmtree('userbot-old')
+	mkdir('userbot-old')
+    rename('userbot', 'userbot-old/userbot')
+    rename('.gitignore', 'userbot-old/.gitignore')
+    rename('LICENSE', 'userbot-old/LICENSE')
+    rename('README.md', 'userbot-old/README.md')
+    rename('requirements.txt', 'userbot-old/requirements.txt')
+    rename('Procfile', 'userbot-old/Procfile')
+    rename('runtime.txt', 'userbot-old/runtime.txt')
+    rename('config.env', 'userbot-old/config.env')
+    rename('userbot.session', 'userbot-old/userbot.session')
+    update = repo.create_remote('sql-extended', off_repo)
+    update.pull('sql-extended')
+    rename('userbot-old/userbot.session', 'userbot.session')
+    rename('userbot-old/config.env', 'config.env')
 
 @register(outgoing=True, pattern="^.update(?: |$)(.*)")
 @errors_handler
@@ -39,6 +58,7 @@ async def upstream(ups):
     "For .update command, check if the bot is up to date, update if specified"
     if not ups.text[0].isalpha() and ups.text[0] not in ("/", "#", "@", "!"):
         await ups.edit("`Checking for updates, please wait....`")
+        initial = False
         conf = ups.pattern_match.group(1)
         off_repo = 'https://github.com/AvinashReddy3108/PaperplaneExtended.git'
 
@@ -49,13 +69,34 @@ async def upstream(ups):
             await ups.edit(f'{txt}\n`directory {error} is not found`')
             return
         except InvalidGitRepositoryError as error:
-            await ups.edit(
-                f'{txt}\n`directory {error} does not seems to be a git repository`'
-            )
-            return
+            repo = Repo.init()
+            initial = True
         except GitCommandError as error:
-            await ups.edit(f'{txt}\n`Early failure! {error}`')
+            await ups.edit(f'{txt}\n`{error}`')
             return
+        
+        if initial:
+            if len(ups.text.split()) != 2:
+                await ups.edit('Your git workdir is missing!\nBut i can repair and take new latest update for you.\nJust do `update now` to repair and take update!')
+                return
+            elif len(ups.text.split()) == 2 and ups.text.split()[1] == "now":
+                try:
+                    await initial_git(repo)
+                except Exception as err:
+                    exc_type, exc_obj, exc_tb = sys.exc_info()
+                    await ups.edit('An error has accured!\nPlease see your Assistant for more information!')
+                    sys.__excepthook__(exc_type, exc_obj, exc_tb)
+                    errors = traceback.format_exception(etype=exc_type, value=exc_obj, tb=exc_tb)
+                    text = "An error has accured!\n\n```{}```\n".format("".join(errors))
+                    if errtype == ModuleNotFoundError:
+                        text += "\nHint: Try this in your terminal `pip install -r requirements.txt`"
+                    if BOTLOG:
+                        await ups.client.send_message(BOTLOG_CHATID, text)
+                    return
+                await ups.edit('Successfully Updated!\nBot is restarting...')
+                await ups.client.disconnect()
+                execl(sys.executable, sys.executable, *sys.argv)
+                exit()
 
         ac_br = repo.active_branch.name
         if not await is_off_br(ac_br):
@@ -72,7 +113,27 @@ async def upstream(ups):
 
         ups_rem = repo.remote('upstream')
         ups_rem.fetch(ac_br)
-        changelog = await gen_chlog(repo, f'HEAD..upstream/{ac_br}')
+        try:
+            changelog = await gen_chlog(repo, f'HEAD..upstream/{ac_br}')
+        except Exception as err:
+            if "fatal: bad revision" in str(err):
+                try:
+                    await initial_git(repo)
+                except Exception as err:
+                    exc_type, exc_obj, exc_tb = sys.exc_info()
+                    await ups.edit('An error has accured!\nPlease see your Assistant for more information!')
+                    sys.__excepthook__(exc_type, exc_obj, exc_tb)
+                    errors = traceback.format_exception(etype=exc_type, value=exc_obj, tb=exc_tb)
+                    text = "An error has accured!\n\n```{}```\n".format("".join(errors))
+                    if errtype == ModuleNotFoundError:
+                        text += "\nHint: Try this in your terminal `pip install -r requirements.txt`"
+                    if BOTLOG:
+                        await ups.client.send_message(BOTLOG_CHATID, text)
+                    return
+                await ups.edit('Successfully Updated!\nBot is restarting...')
+                await ups.client.disconnect()
+                execl(sys.executable, sys.executable, *sys.argv)
+                exit()
 
         if not changelog:
             await ups.edit(
@@ -99,8 +160,11 @@ async def upstream(ups):
             return
 
         await ups.edit('`New update found, updating...`')
-        ups_rem.fetch(ac_br)
-        ups_rem.git.reset('--hard', 'FETCH_HEAD')
+        try:
+            ups_rem.pull(ac_br)
+        except GitCommandError:
+            ups_rem.git.reset('--hard')
+            repo.git.clean('-fd', 'userbot/modules/')
         await ups.edit('`Successfully Updated!\n'
                        'Bot is restarting... Wait for a second!`')
         await ups.client.disconnect()
